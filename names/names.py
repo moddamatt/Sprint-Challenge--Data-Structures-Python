099a4896b26ab9753d81c14629bac60ec4ad5e72
moddamatt <echomatt95@gmail.com>
2019-09-20 18:59:41 -0500

    Names time complexiity reduced to 1.5 seconds

--- names/names.py
+++ names/names.py
@@ -10,11 +10,25 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# Since both files are read into a list i'm going to use a set type.
+
+# Python Docs:
+# intersection(*others)
+# set & other & ...
+# Return a new set with elements common to the set and all others.
+
+# So duplicates will be set to the common intersection of the names list
+# Oh wait nevermind I can't use this hmm
+# duplicates = set(names_1).intersection(names_2)
+
+# Maybe I'll try differnt for loop configurations
+# Declare dups
+duplicates = [name for name in names_1 if name in names_2] # maybe I can use a comprehension
+
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
